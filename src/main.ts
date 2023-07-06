55ae464a8ac4257cc3baf7e466686f3c8fbeb86e
Ariel Rodriguez <ariel.fdr@gmail.com>
2023-07-06 16:53:03 +0200

    GitHub service (#6)
    * wip
    
    * wip
    
    * wip
    
    * wip remove oldies
    
    * Create Github service and pull contents from most recent PR.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,32 @@
 // import { updateInstances } from './logic/update-instances'
-// import { github } from './services/github'
+import { GithubApi } from './services/github-api'
 // import { gpt } from './services/gpt'
 import { checkEnv } from './logic/check-env'
 
 export const main = async () => {
   if (!checkEnv()) return
 
-  console.log('Hello World')
+  const ghApi = new GithubApi({
+    repoName: 'reviewer',
+    repoOwner: 'webbertakken',
+    installationId: '39090441',
+    appId: '353840',
+    privateKey: process.env.GH_APP_CLIENT_SECRET || '',
+  })
 
-  // List instances
-  // await updateInstances(github)
+  try {
+    console.log(`Fetching most recent pull request...`)
+    const recentPullRequest = await ghApi.getMostRecentPullRequest()
+
+    console.log(
+      `Fetching file contents and patches for pull request #${recentPullRequest.number}...`,
+    )
+    const result = await ghApi.getFileChanges(recentPullRequest.number)
+
+    console.log(result)
+  } catch (error) {
+    console.error('Error fetching pull request information:', error)
+  }
+
   // console.log(gpt)
 }
---
+++ src/services/github-api.ts
@@ -0,0 +1,142 @@
+import { Octokit } from '@octokit/rest'
+import { createAppAuth } from '@octokit/auth-app'
+import path from 'path'
+import fs from 'fs'
+
+type GithubApiConfig = {
+  repoName: string
+  repoOwner: string
+  installationId: string
+  appId: string
+  privateKey: string
+}
+
+type fileChange = {
+  filename: string
+  content: string
+  patch: string
+  ignored: boolean
+}
+
+// helpers move it away ...
+// --------------------------------------------
+
+function readGithubPemKey() {
+  const filePath = path.join(__dirname, `../../pr-code-reviewer.private-key.pem`)
+  const fileData = fs.readFileSync(filePath, 'utf8')
+  return fileData
+}
+
+// --------------------------------------------
+
+const ghApiKey = readGithubPemKey()
+
+export class GithubApi {
+  app: any
+  token: string
+  meta: {
+    owner: string
+    repo: string
+  }
+
+  // Authenticates and stores token
+  constructor({ repoName, repoOwner, installationId, appId, privateKey }: GithubApiConfig) {
+    this.meta = {
+      owner: repoOwner,
+      repo: repoName,
+    }
+    this.token = ''
+    console.log('Authenticating with Github App...', privateKey)
+    this.app = new Octokit({
+      authStrategy: createAppAuth,
+      auth: {
+        appId: parseInt(appId, 10),
+        privateKey: ghApiKey,
+        // optional: this will make appOctokit authenticate as app (JWT)
+        //           or installation (access token), depending on the request URL
+        installationId: parseInt(installationId, 10),
+      },
+    })
+  }
+
+  async getMostRecentPullRequest() {
+    try {
+      const response = await this.app.pulls.list({
+        ...this.meta,
+        sort: 'created',
+        direction: 'desc',
+        per_page: 1,
+      })
+
+      const pullRequests = response.data
+      if (!pullRequests.length) {
+        throw new Error('No pull requests found in the repository')
+      }
+      const mostRecentPullRequest = pullRequests[0]
+      return mostRecentPullRequest
+    } catch (error) {
+      console.error('Error fetching pull requests:', error)
+      throw error
+    }
+  }
+
+  async getPullRequestInfo(pullRequestNumber: number): Promise<any> {
+    try {
+      const response = await this.app.pulls.get({
+        ...this.meta,
+        pull_number: pullRequestNumber,
+      })
+
+      return response.data
+    } catch (error) {
+      console.error('Error fetching pull request information:', error)
+      throw error
+    }
+  }
+
+  // Function to fetch file changes from a pull request
+  async getFileChanges(pullRequestNumber: number): Promise<fileChange[]> {
+    try {
+      const response = await this.app.pulls.listFiles({
+        ...this.meta,
+        pull_number: pullRequestNumber,
+      })
+
+      const files = await Promise.all(
+        response.data.map(async (file: any) => {
+          const { filename } = file
+          if (!filename.startsWith('src')) {
+            return {
+              filename,
+              content: '',
+              patch: '',
+              ignored: true,
+            }
+          }
+          return {
+            filename,
+            content: await this.fetchFileContent(file.raw_url),
+            patch: file.patch,
+            ignored: false,
+          }
+        }),
+      )
+
+      return files.filter((file: any) => !file.ignored)
+    } catch (error) {
+      console.error('Error fetching file changes:', error)
+      throw error
+    }
+  }
+
+  // Fetches content from the blob http url
+  async fetchFileContent(httpUrl: string): Promise<string> {
+    try {
+      const response = await this.app.request(httpUrl)
+      return response.data
+    } catch (error) {
+      console.error('Error fetching file content:', error)
+      throw error
+    }
+  }
+}
